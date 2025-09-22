adc5b6a4e3dd4dfd7dd4225d31e1fe91d3cb8d86
yerincho94 <yerincho94@gmail.com>
2025-09-22 09:49:41 +0900

    자료구조 : Map

--- 01_dataStructure.js
+++ 01_dataStructure.js
@@ -25,3 +25,31 @@ const arr = [1, 3, 4];
 console.log(arr); // [1,3,4]
 arr.length = 0; //js에서는 length를 수정가능
 console.log(arr); //length를 0으로 하면 지워진다. -> []
+
+// Map
+/*
+    Object & Map의 차이
+ */
+const dataObj = {
+  name: "cho",
+  job: "developer",
+  age: 20,
+};
+//for in을 사용하던가, Object.entries로 조회가능
+for (const v of Object.entries(dataObj)) {
+  //for (const v of dataObj) { //TypeError: dataObj is not iterable
+  console.log(v);
+}
+
+// Map : 고정된 키 호출 -> 고정적인 값을 받는 저장 형태.
+/*
+    - 다양한 타입의 키를 지원
+    - 관련된 존재 여부 관련된 연산들...
+    -> 자바스크립트는 원래 별도로 지원을 안해주다가 (Object가 충분히 비슷하니까 )
+*/
+const map = new Map(); //비어있는 용기를 하나 만들고.
+map.set("name", "kim"); // key - value를 세팅
+map.set("job", "engineer");
+map.set("age", 50);
+console.log(map);
+console.log(map.get("age"));
